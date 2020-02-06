29e78c1effd136fda3ad402be9810b56ba2274c7
Andrew Buchanan <ajb28@live.unc.edu>
2020-02-05 20:30:31 -0500

    redTiles, fully functional, version 1.0.0

--- tileApp.R
+++ tileApp.R
@@ -1,81 +1,95 @@
 library(shiny)
 library(leaflet)
+library(sf)
 library(tidyverse)
 library(RColorBrewer)
 library(sp)
 library(rgdal)
 
-#Read the wrs tiles 
-wrs <- 
-  st_read('data/in/wrs2_asc_desc.shp')%>%
-  sample_frac(.05)
-
-
-pal <- colorNumeric(palette = "Blues", domain = wrs$PATH )
-test <- leaflet(wrs) %>% addTiles() %>% addPolygons(stroke = TRUE, color = ~pal(PATH))
-
-
-
-
-ui <- fluidPage(
-  
-  titlePanel("World Reference System Satellite Tiles"),
-  leafletOutput('map')
-  
-  )
-  
-
-
-server <- function(input, output){
-  
-  filteredData <- reactive (input$map_shape_click)
+  #Read the wrs tiles 
+  wrs <- 
+    st_read('data/in/wrs2_asc_desc.shp')%>%
+    sample_frac(.05)
   
   
-  observeEvent(input$map_shape_click,{
-    
-    #click variable stores lat, long, and .nonce of the selected tile
-    click <- input$map_shape_click
-    
-    if(is.null(click))
-      return()
-    
-    #Pull the latitude and longitude from click event
-    lat <- click$lat
-    lon <- click$lng
-    
-    #print( (lat == click$lat) )
-    
-    #put lat lon from the click point into their own data frame
-    cords <- as.data.frame(cbind(lon, lat))
-    
-    #convert click point coords into a spatial point (SP) and sets CRS
-    point <- SpatialPoints(cords)
-    proj4string(point) <- CRS("+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs")
-    
-    
-    ##THIS IS WHERE THINGS GET WEIRD
-    
-    
-    #Retrieve the polygons in which the click point resides
-    pnts <-  st_as_sf(point, coords = c('lon' , 'lat'))
-    intersection <-  as.array(st_intersects(pnts, wrs, sparse = FALSE))
-
-    print("\n\nINTERSECTION:")
-    print(intersection)
-
-    
-  }
-    
-  )  
-  output$map <- renderLeaflet(
-    test
-  )
+  pal <- colorNumeric(palette = "Blues", domain = wrs$PATH )
   
-
+  test <- leaflet(wrs) %>% 
+    addTiles() %>% 
+    addPolygons(stroke = TRUE, color = ~pal(PATH), layerId = ~PR)
+  
+  
+  
+  
+  ui <- fluidPage(
+    
+    titlePanel("World Reference System Satellite Tiles"),
+    leafletOutput('map'),
+    actionButton("button", "Refresh Map"),
+    
+        )
+    
+  
+  
+  server <- function(input, output){
+    
+    
+    output$map <- renderLeaflet({
+      
+      leaflet("map", data = wrs)%>%
+        addTiles()%>%
+        setView(lat=10, lng=0, zoom=2) %>% 
+        addPolygons(color = ~pal(PATH), layerId = ~PR, 
+                    highlightOptions = highlightOptions(color ='Red',
+                                                       opacity = 0.7))
+        
+      
+    })
+    
+    
+    observeEvent(input$map_shape_click,{
+      
+      click <- input$map_shape_click
+      latitude <- click$lat
+      longitude <- click$lng
+      pr <- click$id
+      
+      if(is.null(click)) 
+        return() 
+      
+      #Pull the latitude, longitude, and path row (pr) from click event
+      else {
+      
+      leafletProxy("map")%>%
+        clearShapes()%>%
+        addTiles%>%
+        setView(lng = longitude , lat = latitude, zoom = 5) %>%
+        addPolygons(data = wrs[wrs$PR==pr,], color = 'red')
+      }  
+    
+    })
+    
+    #Use a separate observer to refresh the map as needed
+    observe( {
+      
+      proxyMap <- leafletProxy("map")
+      
+      if(input$button){
+        proxyMap%>%
+          clearShapes()%>%
+          addTiles%>%
+          setView(lng = 0 , lat = 10, zoom = 2) %>%
+          addPolygons(data = wrs, color = ~pal(PATH), layerId = ~PR, 
+                      highlightOptions = highlightOptions(color ='Red',
+                                                          opacity = 0.7))
+        
+      }
+      
+      })
           
 }
     
-  
+
 
 
 shinyApp(ui, server)
