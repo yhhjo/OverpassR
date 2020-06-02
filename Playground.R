2128f65751cfe70337bf85ce86252a5aab786214
Andrew Buchanan <ajb28@live.unc.edu>
2020-06-02 16:28:08 -0700

    Progress towards adding Sentinel. Included new shapefile with MGRS grid.

--- Playground.R
+++ Playground.R
@@ -1,21 +1,36 @@
-library(shiny)
 library(leaflet)
 library(tidyverse)
 library(plyr)
 library(dplyr)
 library(mapview)
 library(rgdal)
+library(sf)
 
-parsed <- st_read('Data/In/Sentinel/s2_swaths.shp')
-st_crs(parsed) = 4326
+#Global Variable
+PARSED <- st_read('Data/In/Sentinel/s2_swaths.shp', stringsAsFactors = FALSE)
+st_crs(PARSED) = 4326
+MGRS <- st_read('Data/In/Sentinel/sentinel2_tiles_world.shp', stringsAsFactors = FALSE)
 
-lon <- 10
-lat <- 10
+#Reactive Data
+lon <- 73.1
+lat <- 67.1
+start <- "2020-07-01" %>% as.Date() %>% as.numeric()
+stop <- "2020-07-13" %>% as.Date() %>% as.numeric()
 
 
 click <- cbind(lon, lat) %>% as.data.frame() %>% SpatialPoints() %>% st_as_sf(coords = c('lon','lat'))
 st_crs(click) = 4326
-bool <- st_intersects(click, parsed, sparse = FALSE)
+swath_bool <- st_intersects(click, PARSED, sparse = FALSE)
+mgrs_bool <- st_intersects(click, MGRS, sparse = FALSE)
 
-#Why does this have 19 levels?
-swaths <- parsed[bool,]$Overpass
+ref_date <- PARSED[swath_bool,]$Overpass %>% as.Date() %>% as.numeric()
+
+dates <- data.frame()
+
+for (r in 1:length(ref_date)){
+  first_pass <-  start + ((start - ref_date[r]) %% 5)
+  overpasses <- seq.Date( as.Date(first_pass, '1970-01-01'), as.Date(stop, '1970-01-01'), by = 5) %>% as.data.frame()
+  dates <- rbind(dates, as.character.Date(overpasses)) %>% distinct()
+}
+
+names(dates) <- "Dates"
