9c6e2f80e6cf30882b0d9ad1d066400565d1c14d
Andrew Buchanan <ajb28@live.unc.edu>
2020-08-30 14:42:16 -0700

    Cleaned out junk code

--- Swaths.R
+++ Swaths.R
@@ -2,32 +2,37 @@ library(rgdal)
 library(sf)
 library(tidyverse)
 
+
 #TODO: Fetch swaths and save as kml using Regex search 
 
-#Read kml file: st_read(swaths, layer = nominal)
+#Read kml
 swaths <- st_read('Data/In/Sentinel/Sentinel-2A_MP_ACQ_KML_20200716T120000_20200803T150000.kml', layer = "NOMINAL")
+
+#Columns to be merged with MGRS
 swaths$Overpass <- as.Date(swaths$begin, format = "%y/%m/%d")
-swaths$Times <- strftime(swaths$begin, format = "%H:%M")
+swaths$Time <- strftime(swaths$begin, format = "%H:%M")
 
 #Read MGRS file. Write "Overpass" and "Time" columns as <NA>
 mgrs <- st_read('Data/In/Sentinel/Relevant_MGRS.shp')
 mgrs$Overpass <- NA
 mgrs$Time <- NA
 
-#TODO Clear the unrelated columns
+#TODO Clear the unrelated columns for faster processing
 
-#For each MGRS tile, find the swath(s) it intersects
- ## if one, pull the Overpass and Time info
-## If multiple, find the one it overlaps with most 
-edges <- data.frame("Rows" = NA)
+#For each MGRS tile, find the Swath it most overlaps with, and update "Overpass" and "Time" data from it
 
+
+#SLOW! Better way of merging data than a for-loop?
 for (r in 1:length(mgrs$Name)) { 
   swath <- swaths[st_intersects(mgrs[r,], swaths)[[1]],]
   
-  
+  #Tile overlaps with multiple swaths
   if(length(swath$name) > 1 ) {
-    #TODO: compare areas to find the tile that overlaps most
-    break
-    }
+    #TODO: compare areas to find set swath to one with max overlap.
+    mgrs[r,]$Overpass <- NA
+    mgrs[r,]$Time <- NA
+  }
+  
+  mgrs[r,]$Overpass <- swath$Overpass
+  mgrs[r,]$Time <- swath$Time
 }
-#bool <- ((st_intersection(swaths[1,], mgrs[r,]) %>% st_area()  %>% as.numeric() ) / (st_area(mgrs[r,]) %>% as.numeric())) >.9
