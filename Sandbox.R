e192afb15bd94b76ec9e2dfe5327c31c3024746c
Andrew Buchanan <ajb28@live.unc.edu>
2020-09-16 23:49:12 -0700

    Datetime algorithm works. Need to implement output table merging

--- Sandbox.R
+++ Sandbox.R
@@ -1,3 +1,4 @@
+library(shiny)
 library(sf)
 library(tidyverse)
 library(rgdal)
@@ -8,9 +9,9 @@ library(lubridate)
 SWATHS <- st_read('Data/In/Sentinel/Sentinel-2A_MP_ACQ_KML_20200716T120000_20200803T150000.kml', layer = "NOMINAL")
 MGRS <- st_read('Data/In/Sentinel/Relevant_MGRS.shp')
 
-# Recreated Envitonment Variables ---------
+# Recreated Environment Variables ---------
 start <- today()
-stop <- start + 10 
+stop <- start + 30 
 click <- cbind(5, 60) %>% as.data.frame() %>% SpatialPoints() %>% st_as_sf(coords = c('lon','lat'))
 st_crs(click) = 4326
 
@@ -20,14 +21,23 @@ swaths <- SWATHS[st_intersects(click, SWATHS, sparse = FALSE),]
 mgrs <- MGRS[st_intersects(click, MGRS, sparse = FALSE),]
 
 # Read the Datetime (POSIXct object) from each swath that intersects the user's click. It's stored under swaths$begin.
-begin <- swaths$begin
+begin <- swaths$begin %>% ymd_hms()
 
 # Find the whole number of days between the swath's reference datetime and the start of the user's selected range (date).
+daterange_offset <- (difftime(begin, start) %>% as.integer() %% 5) %>% ddays()
 
-# Modulo that whole number by 5 [sentinel return period], and add the result to the start of the date range to get the first overpass within the user's selected range.
-# Add the time from the swaths's reference Datetime to this first overpass within range
-# Increment first overpass by 5 days while preserving the time until a datetime is outside of the date range.
-# Save all of these 5-day increments as Datetime objects.
-# Merge this array of Datetime objects with the array of all MGRS tiles that intersect the click (creates all possible tile-datetime combos since Sentinel acquires images by swath, not by tile).
+# Datetime objects within user selected range
+dates <-  start + daterange_offset
+times <- strftime(begin, format = "%H:%M:%S", tz = "UTC") %>% hms()
+overpass <- data.frame("Overpass" = NULL)
+
+# Loops roughly the  
+for(i in 1:length(dates)) {
+  x <- seq.Date(dates[i], stop, 5) + times[i]
+  x <- data.frame("Overpass" = x[x <= stop])
+  overpass <- rbind(overpass, x)
+}
+
+
+#TODO: 
 # Generate the output table.
-
