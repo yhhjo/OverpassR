c7dd404a5f2099393f347b442d2c5bc3910c15c3
Andrew Buchanan <ajb28@live.unc.edu>
2020-10-05 13:39:02 -0700

    Error handling built in

--- Swaths.R
+++ Swaths.R
@@ -5,32 +5,55 @@ library(tidyverse)
 #For web scraping
 library(xml2)
 library(rvest)
-
-url <- 'https://sentinel.esa.int/web/sentinel/missions/sentinel-2/acquisition-plans'
-
-# Pull HTML and CSS source code
-page <- read_html(url)
-
-# Pulls specific attribute within webpage that contains the latest sentinel-2a acquisition swath URL extension
-link <- (page %>% html_nodes(".sentinel-2a") %>% html_nodes("a"))[1] %>% as.character()
-
-# Regex: match everything between "/d" and .kml on the hyperlink attribute
-regex <- "/d(.*)\\.kml"
-
-# Extracts the extension
-extension <- regmatches(link, regexpr(regex, link))
-
-# Full URL for the latest ESA Sentinel 2A acquisition swath
-url <- paste0("https://sentinel.esa.int", extension)
+library(mapview)
 
 
-#TODO: Download KML file from retrieve_url, name it something generic 
-download.file(url, method = "curl", "Latest_Swaths.kml", destfile = "Data/In/Sentinel")
-## TODO: Handle any link download errors 
-
-#TODO: Ensure downloaded file works and isn't corrupted
-## TODO: can open st_read, make sure it has layer "NOMINAL", and has at least 50 attributes
-
-#TODO: Replace the old file wuth new one
-
+update_swaths <- function() {
+  
+  # Fetch if today is more than 10 days over the latest acquisition SWATH
+  if (!(SWATHS$begin %>% as.Date() %>% max() - today()) < -10) {
+    return()
+  } else {
+    
+    out <- tryCatch(
+      
+      {
+        # === EXPR block of trycatch function ===
+        url <-
+          'https://sentinel.esa.int/web/sentinel/missions/sentinel-2/acquisition-plans'
+        
+        # Pull HTML and CSS source code
+        page <- read_html(url)
+        
+        # Pulls specific attribute within webpage that contains the latest sentinel-2a acquisition swath URL extension
+        link <-
+          (page %>% html_nodes(".sentinel-2a") %>% html_nodes("a"))[1] %>% as.character()
+        
+        # Regex: match everything between "/d" and .kml on the hyperlink attribute
+        regex <- "/d(.*)\\.kml"
+        
+        # Extracts the extension
+        extension <- regmatches(link, regexpr(regex, link))
+        url <- paste0("https://sentinel.esa.int", extension)
+        
+        # Download kml as temp file
+        download.file(url, "temp.kml")
+        stopifnot(file.rename("temp.kml", "./Data/In/Sentinel/Swaths.kml"))
+      
+        # Update local data
+        SWATHS <<- st_read("Data/In/Sentinel/Swaths.kml", layer = "NOMINAL")
+      },
+      
+      # === WARNING HANDLING ===
+      warning = function(cond) {
+        output$helpText <- renderText("A warning flag was raised while updating ESA Sentinel 2 Database. Please report this bug to ajb28@live.unc.edu ")
+      },
+      
+      # === ERROR HANDLING  ===
+      error = function(cond) {
+        output$helpText <- renderText("An error occurred connecting to ESA Sentinel 2 Database. Please report this bug to ajb28@live.unc.edu ")
+      }
+    )
+  }
+}
 
